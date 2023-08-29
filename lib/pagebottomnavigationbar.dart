7d33c2501b1069f6617cbedd20c5927d4c3613e5
Dr. Raed <drraed.jb@gmail.com>
2023-08-29 17:16:14 +0200

    _facebookWidge

--- lib/pagebottomnavigationbar.dart
+++ lib/pagebottomnavigationbar.dart
@@ -1,10 +1,72 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:tutorialbottomnavigatiombar/cubitforbottomnavigationbar.dart';
 
 class PageBottomNavigationBar extends StatelessWidget {
   const PageBottomNavigationBar({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("An example of BottomNavigationBar"),
+         backgroundColor: Colors.pink,
+      ),
+      body:BlocBuilder<CubitForBottomNavigationBar,int>(
+        builder: ((context, state) {
+          if(state==0)
+          {
+            return _emailWidge();
+            } else if(state==1
+            )
+          {
+             return _facebookWidge();
+          }else{
+            return _telegramWidge();
+          }
+          
+        })
+    );
   }
+
+  Widget _emailWidge()=>Container(
+              margin:const EdgeInsets.all(50),
+              color: Colors.red,
+              height: 100,
+              width: 100,
+              child:const Column(
+                children: [
+                  Text("Mail"),
+                   Icon(Icons.mail),
+                ]
+                ),
+            );
+
+     Widget _facebookWidge()=>Container(
+              margin:const EdgeInsets.all(50),
+              color: Colors.blue,
+              height: 100,
+              width: 100,
+              child:const Column(
+                children: [
+                  Text("Facebook"),
+                   Icon(Icons.facebook),
+                ]
+                ),
+            );  
+    Widget _telegramWidge()=>Container(
+              margin:const EdgeInsets.all(50),
+              color: Colors.green,
+              height: 100,
+              width: 100,
+              child:const Column(
+                children: [
+                  Text("Telegram"),
+                   Icon(Icons.telegram),
+                ]
+                ),
+            );           
 }
+
